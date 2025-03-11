b9d335a55971fdc014568280cd5613663f799cf5
onkar ungratwar <ungratwaronkar@gmail.com>
2025-03-11 16:10:34 +0530

    changes

--- src/validations/homework.validation.js
+++ src/validations/homework.validation.js
@@ -79,6 +79,19 @@ const getHomeworkFilter = {
   }),
 };
 
+const getHomeByFilter = {
+  body: Joi.object().keys({
+    boardId: Joi.string().custom(objectId),
+    mediumId: Joi.string().custom(objectId),
+    classId: Joi.string().custom(objectId),
+    subjectId: Joi.string().custom(objectId),
+    bookId: Joi.string().custom(objectId),
+    chapterId: Joi.string().custom(objectId),
+    search: Joi.string(),
+    limit: Joi.number().integer().min(1).default(10),
+    page: Joi.number().integer().min(1).default(1),
+  }),
+};
 module.exports = {
   createHomework,
   getAllHomework,
@@ -87,4 +100,5 @@ module.exports = {
   updateHomework,
   deleteHomework,
   getHomeworkFilter,
+  getHomeByFilter
 };
